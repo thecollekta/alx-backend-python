c50ffeda36fe1f106b4673c34b32d7c771dcec89
thecollekta <festus233@gmail.com>
2025-07-17 12:09:47 +0000

    Updated the settings.py to include DRF authentication and permission classes

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -133,3 +133,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# REST Framework Settings
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+}
