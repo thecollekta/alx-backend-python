fbc2b3ea1d58251999b638ffc3c1cc35cdaf29a2
thecollekta <festus233@gmail.com>
2025-07-19 14:19:41 +0000

    feat(api): updated urls.py to include routers

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -23,4 +23,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("chats.urls")),
+    path('api-auth/', include('rest_framework.urls')),  # DRF auth for browseable API
 ]
